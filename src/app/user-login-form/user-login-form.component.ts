1e07f565d1bb616c5a973ab2b545c68e05096fff
philo <ducassephi@hotmail.fr>
2024-02-29 09:09:49 +0100

    added default login credentials

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -19,7 +19,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./user-login-form.component.scss']
 })
 export class UserLoginFormComponent implements OnInit {
-  @Input() userData = { Username: '', Password: '' };
+  @Input() userData = { Username: 'testuser', Password: 'password' };
   
   constructor(
     public fetchApiData: FetchApiDataService,
